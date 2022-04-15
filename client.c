c1c68d5bb035a98cf5d0bb80fb83e4a0341a2155
Feemoyo <lipemoreira.yorgos@gmail.com>
2022-04-15 23:15:25 +0200

    socorro deus dnv parte 3

--- client.c
+++ client.c
@@ -6,52 +6,39 @@
 /*   By: fmoreira <fmoreira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 21:39:47 by fmoreira          #+#    #+#             */
-/*   Updated: 2022/04/14 19:14:43 by fmoreira         ###   ########.fr       */
+/*   Updated: 2022/04/15 23:14:32 by fmoreira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void ft_despatch(int pid, int bit, int count)
+void ft_despatch(int pid, int bit)
 {
 	if (bit == 0)
 	{
-		printf("bit: %d\n", bit);
+		//printf("bit: %d\n", bit);
 		kill(pid, SIGUSR1);
-		sleep(0.00001);
+		//sleep(0.00009);
 	}
-	if (bit == 1)
+	else if (bit == 1)
 	{
-		printf("bit: %d\n", bit);
+		//printf("bit: %d\n", bit);
 		kill(pid, SIGUSR2);
-		sleep(0.00001);
+		//sleep(0.00009);
 	}
+	sleep(0.01);
 }
 
 void ft_lobby(int pid, t_fake_byte byte)
 {
-	static int count;
-	
-	count = 1;
-	if (count++ == 1)
-		ft_despatch(pid, byte.bit1, count);
-	if (count++ == 2)
-		ft_despatch(pid, byte.bit2, count);
-	if (count++ == 3)
-		ft_despatch(pid, byte.bit3, count);
-	if (count++ == 4)
-		ft_despatch(pid, byte.bit4, count);
-	if (count++ == 5)
-		ft_despatch(pid, byte.bit5, count);
-	if (count++ == 6)
-		ft_despatch(pid, byte.bit6, count);
-	if (count++ == 7)
-		ft_despatch(pid, byte.bit7, count);
-	if (count == 8)
-	{
-		ft_despatch(pid, byte.bit8, count);
-		count = 1;
-	}
+	ft_despatch(pid, byte.bit1);
+	ft_despatch(pid, byte.bit2);
+	ft_despatch(pid, byte.bit3);
+	ft_despatch(pid, byte.bit4);
+	ft_despatch(pid, byte.bit5);
+	ft_despatch(pid, byte.bit6);
+	ft_despatch(pid, byte.bit7);
+	ft_despatch(pid, byte.bit8);
 }
 
 int main(int argc, char **argv)
--- server.c
+++ server.c
@@ -6,62 +6,61 @@
 /*   By: fmoreira <fmoreira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 15:10:46 by fmoreira          #+#    #+#             */
-/*   Updated: 2022/04/14 20:55:06 by fmoreira         ###   ########.fr       */
+/*   Updated: 2022/04/15 21:54:40 by fmoreira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void ft_welcome(int *count, int *value)
+void ft_welcome(int *count, int *value, char bit)
 {
-	if (*count == 1)
+	if (*count == 1 && bit == 1)
+	{
+		*value = 0;
 		*value = *value + 1;
-	else if (*count == 2)
+	}
+	else if (*count == 2 && bit == 1)
 		*value = *value + 2;
-	else if (*count == 3)
+	else if (*count == 3 && bit == 1)
 		*value = *value + 4;
-	else if (*count == 4)
+	else if (*count == 4 && bit == 1)
 		*value = *value + 8;
-	else if (*count == 5)
+	else if (*count == 5 && bit == 1)
 		*value = *value + 16;
-	else if (*count == 6)
+	else if (*count == 6 && bit == 1)
 		*value = *value + 32;
-	else if (*count == 7)
+	else if (*count == 7 && bit == 1)
 		*value = *value + 64;
 	else if (*count == 8)
-		*value = *value + 128;
-	//printf("'char': %d %c\n", *value, *value);
+	{
+		ft_putchar_fd((unsigned char )*value, 1);
+		*count = 0;
+		*value = 0;
+	}
+	//ft_putnbr_fd(*count, 1);
+	//write(1, "\n", 1);
 }
 
 void ft_handler(int signum)
 {
 	static int count;
-	int value;
+	static int value;
 	
-	if(!count || count > 8)
+	if(signum == 10) // SIGUSR1 == 0
 	{
-		value = 0;
-		count = 1;
-	}
-	/*if(!value || value > 127)
-		value = 0; */
-	//printf("count: %d\n", count);
-	if(signum == 10)
-	{
-		//printf("sigusr1\n");
 		count = count + 1;
+		ft_welcome(&count, &value, 0);
 	}
-	else if(signum == 12)
+	else if(signum == 12) // SIGUSR2 == 1
 	{
-		//printf("sigusr2\n");
-		ft_welcome(&count, &value);
 		count = count + 1;
+		ft_welcome(&count, &value, 1);
 	}
-	if(count >= 8)
+	/*if(count > 7)
 		{
 			ft_putchar_fd((unsigned char)value, 1);
 			count = 0;
-		}
+		} */
 }
 
 int main()
@@ -75,7 +74,7 @@ int main()
 	write(1, "\n", 1);
 	act.sa_handler = &ft_handler;
 	sigemptyset(&act.sa_mask);
-	act.sa_flags = SA_RESTART;
+	act.sa_flags = SA_SIGINFO;
 	sigaction(SIGUSR1, &act, NULL);
 	sigaction(SIGUSR2, &act, NULL);
 	while (42)
